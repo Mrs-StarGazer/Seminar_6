27571fb6120f98f91c61a2ab01ab0304f7b6b469
Maria <m.shklyaeva96@gmail.com>
2022-12-24 00:14:45 +0500

    add task 41

---
+++ task_41/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("введите количество чисел");
+
+int numberCount = int.Parse(Console.ReadLine()!);
+Console.WriteLine(CountPisitiveNumbers(numberCount));
+
+int CountPisitiveNumbers(int numberCount)
+{
+    int counter = 0;
+    for (int i = 0; i < numberCount; i++)
+    {
+        int currentNumber = int.Parse(Console.ReadLine()!);
+
+        if (IsPositive(currentNumber))
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+bool IsPositive(int number)
+{
+    if (number > 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
