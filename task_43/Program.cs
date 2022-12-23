69935e778ce014fa9eec69967171d3b968ddeb60
Maria <m.shklyaeva96@gmail.com>
2022-12-24 00:43:07 +0500

    add task 43

---
+++ task_43/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("введите чисела");
+double b1 = double.Parse(Console.ReadLine()!);
+double b2 = double.Parse(Console.ReadLine()!);
+double k1 = double.Parse(Console.ReadLine()!);
+double k2 = double.Parse(Console.ReadLine()!);
+
+double[] intersectPoint = GetIntersectPoint(b1, b2, k1, k2);
+
+Console.WriteLine(intersectPoint[0]);
+Console.WriteLine(intersectPoint[1]);
+
+double[] GetIntersectPoint(double b1, double b2, double k1, double k2)
+{
+    double[] result = new double[2];
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+
+    result[0] = x;
+    result[1] = y;
+
+    return result;
+}
